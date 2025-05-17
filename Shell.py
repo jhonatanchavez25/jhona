b1f9ecb90efbd4714de6eb87b7cd19c3d2887405
Jhona <chavezjhonatan99@gmail.com>
2025-05-16 19:29:20 -0500

    agrego botones

--- Shell.py
+++ Shell.py
@@ -1,7 +1,9 @@
+#Importamos librerias
 import tkinter as tk
 from PIL import Image, ImageTk
 import subprocess
 import webbrowser
+
 
 def abrir_terminal():
     subprocess.Popen("start cmd", shell=True)
@@ -25,3 +27,12 @@ def cargar_imagen(ruta):
 img_terminal = cargar_imagen("C:/Users/Oscar Eduardo Lozada/OneDrive - KONSTRUIR/Documentos/sistemas operativos/WINDOS-TERMINAL.png")
 img_navegador = cargar_imagen("C:/Users/Oscar Eduardo Lozada/OneDrive - KONSTRUIR/Documentos/sistemas operativos/unnamed.png")
 img_calculadora = cargar_imagen("C:/Users/Oscar Eduardo Lozada/OneDrive - KONSTRUIR/Documentos/sistemas operativos/calculadora-casio-mx-12b-bk.jpg")
+
+btn_terminal = tk.Button(root, image=img_terminal, command=abrir_terminal)
+btn_navegador = tk.Button(root, image=img_navegador, command=abrir_navegador)
+btn_calculadora = tk.Button(root, image=img_calculadora, command=abrir_calculadora)
+
+btn_terminal.grid(row=0, column=0, padx=20, pady=20)
+btn_navegador.grid(row=0, column=1, padx=20, pady=20)
+btn_calculadora.grid(row=0, column=2, padx=20, pady=20)
+root.mainloop()
