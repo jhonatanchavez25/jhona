1957d6915bc0491953ceb846244c4fe86ac214ff
Jhona <chavezjhonatan99@gmail.com>
2025-05-16 19:22:31 -0500

    link imagenes

--- Shell.py
+++ Shell.py
@@ -16,3 +16,12 @@ root = tk.Tk()
 root.title("shell Grafico(Sistemas operativos)")
 root.geometry("450x200")
 root.resizable(False,False)
+
+def cargar_imagen(ruta):
+    img = Image.open(ruta)
+    img = img.resize((100, 100), Image.LANCZOS)
+    return ImageTk.PhotoImage(img)
+
+img_terminal = cargar_imagen("C:/Users/Oscar Eduardo Lozada/OneDrive - KONSTRUIR/Documentos/sistemas operativos/WINDOS-TERMINAL.png")
+img_navegador = cargar_imagen("C:/Users/Oscar Eduardo Lozada/OneDrive - KONSTRUIR/Documentos/sistemas operativos/unnamed.png")
+img_calculadora = cargar_imagen("C:/Users/Oscar Eduardo Lozada/OneDrive - KONSTRUIR/Documentos/sistemas operativos/calculadora-casio-mx-12b-bk.jpg")
