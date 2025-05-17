f35391ad935582f0e5c0cbdef42e1790c4d214e3
Jhona <chavezjhonatan99@gmail.com>
2025-05-16 19:17:08 -0500

    defino imagen

--- Shell.py
+++ Shell.py
@@ -3,3 +3,16 @@ from PIL import Image, ImageTk
 import subprocess
 import webbrowser
 
+def abrir_terminal():
+    subprocess.Popen("start cmd", shell=True)
+
+def abrir_navegador():
+    webbrowser.open("https://www.google.com")
+
+def abrir_calculadora():
+    subprocess.Popen("calc")
+
+root = tk.Tk()
+root.title("shell Grafico(Sistemas operativos)")
+root.geometry("450x200")
+root.resizable(False,False)
